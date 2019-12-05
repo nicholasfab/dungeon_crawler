3b72c57165ba60a96e58f7b061f1bc41f91c7a82
Nicholas Fabella <nicholasfabella@gmail.com>
2019-12-05 10:27:51 -0500

    Add files via upload

---
+++ tester.py
@@ -0,0 +1,43 @@
+import time
+import random
+
+weapon_list = ["sword", "bow", "rock"]
+item_list = ["potion", "arrow", "stick"]
+
+##class Inventory:
+##    global weapon_list
+##    global item_list
+##
+##    
+##    global name
+##    global damage
+##
+##    
+##    def __init__(self, name):
+##        self.name = name
+##        self.weapons = []
+##        self.items = []
+##
+##    def add_weapon(self, weapon):
+##        self.weapons.append(weapon)
+##
+##    def add_item(self, item):
+##        self.items.append(item)
+        
+class object:
+
+        
+def main():
+    test = Inventory(name)
+
+def start():
+    global name
+    global damage
+    
+    name = input("name weapon: ")
+    damage = input("set damage: ")
+
+    main()
+
+
+start()
